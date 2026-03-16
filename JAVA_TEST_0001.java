864b758554e3680d1cf3c3e406c3555d827a6fb5
Shrish Diwate <shrishdiwate05@gmail.com>
2026-03-16 11:50:20 +0530

    Add files via upload

---
+++ JAVA_TEST_0001.java
@@ -0,0 +1,37 @@
+public class JAVA_TEST_0001 {
+
+    public static void main(String[] args) {
+        int n=5;
+
+        for (int i=0 ; i<n ; i++){
+            for (int j= 0 ; j < (n-i-1) ; j++){
+                System.out.print(" ");
+            }
+
+            System.out.print("*");
+            if(i> 0) {
+                for (int j = 0; j < (2 *i -1); j++) {
+                    System.out.print(" ");
+                }
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        for (int i =n-2; i>=0; i--) {
+            for (int j=0; j < (n - 1 - i); j++) {
+                System.out.print(" ");
+            }
+            System.out.print("*");
+
+            if (i > 0) {
+                for (int s = 0; s < (2 * i - 1); s++) {
+                    System.out.print(" ");
+                }
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
